1a6d094c8816ef123ba33020057449e60f5740cb
ANkit Dware <sabindware@gmail.com>
2024-12-23 08:48:29 +0545

    dfs search and iteration using stack

--- Algorithms/BinaryTree/dfs.cpp
+++ Algorithms/BinaryTree/dfs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stack>
 
 struct Node {
 
@@ -41,6 +42,36 @@ std::cout << root->data << std::endl;
 return;
 }
 
+void iterative_dfs(Node* root){
+std::stack<Node*> stack;
+stack.push(root);
+
+while(!stack.empty()){
+Node* node = stack.top();
+std::cout << node->data << std::endl;
+stack.pop();
+if(node->right != NULL){
+stack.push(node->right);
+}
+if(node->left != NULL){
+stack.push(node->left);
+}
+
+}
+}
+
+bool dfs_search(Node* node,int value){
+if(node == NULL){
+return false;
+}
+else if(node->data == value){
+return true;
+}
+else{
+return dfs_search(node->left, value) || dfs_search(node->right, value);
+}
+}	
+
 int main(){
 Node* root = new Node(5);
 Node* node2 = new Node(2);
@@ -68,6 +99,11 @@ node3->left = node6;
 preorderTraversal(root);
 inorderTraversal(root);
 postorderTraversal(root);
+
+iterative_dfs(root);
+
+std::cout << dfs_search(root, 2) << std::endl;
+std::cout << dfs_search(root, 11) << std::endl;
 return 0;
 }
 
